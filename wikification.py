6049a853aa29854cadc6ea20012653adf225403b
catb0y <mgn.palandri@gmail.com>
2020-03-28 14:33:24 +0100

    Failed collapsing approach, box for wiki info, links, perfected the rest

--- wikification.py
+++ wikification.py
@@ -1,36 +1,29 @@
 import requests
 import json
 import wikipedia
+from wikipedia import DisambiguationError
 
 API_BASE = "http://api.dbpedia-spotlight.org/en"
 endpoint = "/annotate"
-
-# Call endpoints for annotate URIs
-def get_wiki_annotation(text):
-    params = {
-        "text": text["label"],
-        "confidence": 0
-    }
-    response = requests.get(API_BASE+endpoint, headers={"accept": "application/json"},  params=params).json()
-    uris = []
-    if "Resources" in response:
-        for resource in response["Resources"]:
-            uris.append(resource.get("@URI"))
-        info = []
-        for uri in uris:
-            info.append(requests.get(uri))
-        return info
 
 
 # Integrate texts w/ presentation of texts
 def integrate_wiki(keyword):
     keyword_page = keyword["label"]
-    return wikipedia.summary(keyword_page)
+    wiki_summary = ""
+    try:
+        wiki_summary = wikipedia.summary(keyword_page)
+    except DisambiguationError:
+        pass
+    return wiki_summary, "https://en.wikipedia.org/wiki/{}".format(keyword_page), keyword_page
 
 
-def find_related_wiki(keyword):
-    keyword_page = keyword["label"]
-    related = wikipedia.search(keyword_page)[1:4]
-    return related  # list of keywords
-
-
+def find_related_wiki(keyword, elements):
+    elements_nodes = [d["data"].get("label") for d in elements]
+    try:
+        keyword_page = keyword["label"]
+        related = wikipedia.search(keyword_page)[1:]
+        related = [label for label in related if label not in elements_nodes][0:3]
+    except DisambiguationError:
+        return []
+    return related  # list of related wikipedia articles
